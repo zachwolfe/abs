77b7ea4167cfa981522d254f18ad3617a5fdbfbc
Zach Wolfe <zachrwolfe@me.com>
2021-04-24 18:41:47 -0500

    Link.

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,13 @@ enum CxxStandard {
 
 struct Environment {
     compiler_flags: Vec<String>,
+    linker_flags: Vec<String>,
     compiler_path: PathBuf,
     linker_path: PathBuf,
 }
 
 #[derive(Debug)]
-struct CompilerError {
+struct BuildError {
     code: Option<i32>,
     message: String,
 }
@@ -41,6 +42,7 @@ impl Environment {
         compile_mode: CompileMode,
         options: CxxOptions,
         include_paths: impl IntoIterator<Item=impl AsRef<Path>>,
+        lib_paths: impl IntoIterator<Item=impl AsRef<Path>>,
         definitions: impl IntoIterator<Item=impl AsRef<str>>,
         compiler_path: impl Into<PathBuf>, linker_path: impl Into<PathBuf>
     ) -> Self {
@@ -81,19 +83,32 @@ impl Environment {
                 flags
             },
         };
+        let linker_flags = match host {
+            Host::Windows => {
+                let mut flags = vec![
+                    "/nologo".to_string(),
+                    "/debug".to_string(),
+                    "/subsystem:windows".to_string(),
+                ];
+                for path in lib_paths {
+                    flags.push(format!("/LIBPATH:{}", path.as_ref().to_str().unwrap()));
+                }
+                flags
+            }
+        };
         Environment {
             compiler_flags,
+            linker_flags,
             compiler_path: compiler_path.into(),
             linker_path: linker_path.into(),
         }
     }
 
-    fn compile(&self, path: impl AsRef<Path>, obj_path: impl AsRef<Path>) -> Result<String, CompilerError> {
+    fn compile(&self, path: impl AsRef<Path>, obj_path: impl AsRef<Path>) -> Result<String, BuildError> {
         let mut args = self.compiler_flags.clone();
         let path = path.as_ref();
-        let obj_path = obj_path.as_ref().to_str().unwrap();
-        args.push(format!(r"/Fo{}\", obj_path));
-        args.push(format!(r"/Fd{}\{}.pdb", obj_path, path.file_stem().unwrap().to_str().unwrap().to_string()));
+        args.push(format!(r"/Fo{}", get_artifact_path(path, &obj_path, "obj").to_str().unwrap()));
+        args.push(format!(r"/Fd{}", get_artifact_path(path, &obj_path, "pdb").to_str().unwrap()));
         args.push(path.to_str().unwrap().to_string());
         let output = Command::new(&self.compiler_path)
             .args(&args)
@@ -103,12 +118,51 @@ impl Environment {
         if output.status.success() {
             Ok(stdout)
         } else {
-            Err(CompilerError {
+            Err(BuildError {
                 code: output.status.code(),
                 message: stdout
             })
         }
     }
+
+    fn link(
+        &self,
+        output_path: impl AsRef<Path>,
+        obj_paths: impl IntoIterator<Item=impl AsRef<Path>>,
+        lib_paths: impl IntoIterator<Item=impl AsRef<Path>>,
+    ) -> Result<String, BuildError> {
+        let mut args = self.linker_flags.clone();
+        let mut output_path = output_path.as_ref().to_owned();
+        output_path.push("main.exe");
+        args.push(format!("/out:{}", output_path.to_str().unwrap().to_string()));
+        for path in obj_paths {
+            args.push(path.as_ref().to_str().unwrap().to_string());
+        }
+        for path in lib_paths {
+            args.push(path.as_ref().to_str().unwrap().to_string());
+        }
+        let output = Command::new(&self.linker_path)
+            .args(&args)
+            .output()
+            .expect("failed to execute process");
+        let stdout = std::str::from_utf8(&output.stdout).unwrap().to_string();
+        if output.status.success() {
+            Ok(stdout)
+        } else {
+            Err(BuildError {
+                code: output.status.code(),
+                message: stdout
+            })
+        }
+    }
+}
+
+fn get_artifact_path(src_path: impl AsRef<Path>, output_dir_path: impl AsRef<Path>, extension: &'static str) -> PathBuf {
+    let mut path = output_dir_path.as_ref().to_owned();
+    path.push(src_path.as_ref().file_stem().unwrap().to_str().unwrap().to_string());
+    let succ = path.set_extension(extension);
+    assert!(succ);
+    path
 }
 
 struct CxxOptions {
@@ -146,6 +200,12 @@ fn main() {
             r"C:\Program Files (x86)\Windows Kits\10\include\10.0.18362.0\um",
             r"C:\Program Files (x86)\Windows Kits\10\include\10.0.18362.0\winrt",
             r"C:\Program Files (x86)\Windows Kits\10\include\10.0.18362.0\cppwinrt",
+        ],
+        &[
+            r"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\ATLMFC\lib\x86",
+            r"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\lib\x86",
+            r"C:\Program Files (x86)\Windows Kits\10\lib\10.0.18362.0\ucrt\x86",
+            r"C:\Program Files (x86)\Windows Kits\10\lib\10.0.18362.0\um\x86",
         ],
         &["_WINDOWS", "WIN32", "UNICODE", "_USE_MATH_DEFINES"],
         compiler_path,
@@ -195,9 +255,13 @@ fn main() {
     }).collect();
 
     let mut success = true;
+    let mut obj_paths = Vec::new();
     for (i, path) in src_paths.iter().enumerate() {
         match env.compile(path, &obj_path) {
-            Ok(message) => print!("Compiled {}", message),
+            Ok(message) => {
+                print!("Compiled {}", message);
+                obj_paths.push(get_artifact_path(path, &obj_path, "obj"));
+            },
             Err(error) => {
                 println!(
                     "Attempted to compile {}Compilation failed{}.{}",
@@ -217,10 +281,15 @@ fn main() {
             }
         }
     }
+    let lib_paths = &["avrt.lib"];
+    if let Some(error) = env.link(&artifact_path, obj_paths, lib_paths).err() {
+        print!("{}", error.message);
+        success = false;
+    }
     if success {
         println!("\nBuild succeeded.");
     } else {
         println!("\nBuild failed.");
-        std::process::exit(2);
+        std::process::exit(1);
     }
 }
