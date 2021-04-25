2f8b82c2c08a37ca8793537bde3215f0ce0d899a
Zach Wolfe <zachrwolfe@me.com>
2021-04-24 23:45:09 -0500

    Recursively compile subdirectories

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::path::{Path, PathBuf};
 use std::convert::AsRef;
 use std::fs;
 use std::ffi::OsStr;
-use std::io::ErrorKind as IoErrorKind;
+use std::io::{self, ErrorKind as IoErrorKind};
 use std::os::windows::prelude::*;
 
 enum Host {
@@ -28,6 +28,9 @@ struct Environment {
     linker_flags: Vec<String>,
     compiler_path: PathBuf,
     linker_path: PathBuf,
+
+    src_dir_path: PathBuf,
+    objs_path: PathBuf,
 }
 
 #[derive(Debug)]
@@ -45,7 +48,9 @@ impl Environment {
         include_paths: impl IntoIterator<Item=impl AsRef<Path>>,
         lib_paths: impl IntoIterator<Item=impl AsRef<Path>>,
         definitions: impl IntoIterator<Item=impl AsRef<str>>,
-        compiler_path: impl Into<PathBuf>, linker_path: impl Into<PathBuf>
+        compiler_path: impl Into<PathBuf>, linker_path: impl Into<PathBuf>,
+        src_dir_path: impl Into<PathBuf>,
+        objs_path: impl Into<PathBuf>,
     ) -> Self {
         let compiler_flags = match host {
             Host::Windows => {
@@ -102,14 +107,17 @@ impl Environment {
             linker_flags,
             compiler_path: compiler_path.into(),
             linker_path: linker_path.into(),
+            src_dir_path: src_dir_path.into(),
+            objs_path: objs_path.into(),
         }
     }
 
     fn compile(&self, path: impl AsRef<Path>, obj_path: impl AsRef<Path>) -> Result<String, BuildError> {
         let mut args = self.compiler_flags.clone();
         let path = path.as_ref();
-        args.push(format!(r"/Fo{}", get_artifact_path(path, &obj_path, "obj").to_str().unwrap()));
-        args.push(format!(r"/Fd{}", get_artifact_path(path, &obj_path, "pdb").to_str().unwrap()));
+        let relative_path = path.strip_prefix(&self.src_dir_path).unwrap();
+        args.push(format!(r"/Fo{}", get_artifact_path(relative_path, &obj_path, "obj").to_str().unwrap()));
+        args.push(format!(r"/Fd{}", get_artifact_path(relative_path, &obj_path, "pdb").to_str().unwrap()));
         args.push(path.to_str().unwrap().to_string());
         let output = Command::new(&self.compiler_path)
             .args(&args)
@@ -156,11 +164,89 @@ impl Environment {
             })
         }
     }
+
+    fn compile_directory(&self, paths: &Paths, newest_header: u64, obj_paths: &mut Vec<PathBuf>) -> bool {
+        let mut success = true;
+        macro_rules! fail {
+            ($($t:tt)*) => {
+                println!($($t)*);
+                success = false;
+            }
+        }
+        fs::create_dir_all(&paths.root).unwrap();
+        for path in paths.src_paths.iter() {
+            let obj_path = get_artifact_path(path.strip_prefix(&self.src_dir_path).unwrap(), &self.objs_path, "obj");
+            obj_paths.push(obj_path.clone());
+            let mut needs_compile = true;
+            let src_modified = fs::metadata(path).unwrap().last_write_time();
+            if let Ok(metadata) = fs::metadata(&obj_path) {
+                let obj_modified = metadata.last_write_time();
+                if obj_modified > newest_header && obj_modified > src_modified {
+                    needs_compile = false;
+                }
+            }
+            if needs_compile {
+                let mut obj_subdir_path = obj_path;
+                obj_subdir_path.pop();
+                fs::create_dir_all(&obj_subdir_path).unwrap();
+                match self.compile(path, &self.objs_path) {
+                    Ok(message) => print!("Compiled {}", message),
+                    Err(error) => {
+                        fail!(
+                            "Attempted to compile {}Compilation failed{}.",
+                            error.message,
+                            if let Some(code) = error.code {
+                                format!(" with code {}", code)
+                            } else {
+                                String::new()
+                            },
+                        );
+                    }
+                }
+            }
+        }
+
+        for child in &paths.children {
+            success &= self.compile_directory(child, newest_header, obj_paths);
+        }
+
+        success
+    }
+}
+
+#[derive(Default)]
+struct Paths {
+    root: PathBuf,
+    src_paths: Vec<PathBuf>,
+    children: Vec<Paths>,
+}
+
+fn header_and_src_paths(root: PathBuf, header_paths: &mut Vec<PathBuf>, entries: impl IntoIterator<Item=io::Result<fs::DirEntry>>) -> Paths {
+    let mut paths = Paths::default();
+    paths.root = root;
+    for entry in entries {
+        let entry = entry.unwrap();
+        let file_type = entry.file_type().unwrap();
+        if file_type.is_file() {
+            let path = entry.path();
+            if path.extension() == Some(OsStr::new("cpp")) || path.extension() == Some(OsStr::new("cxx")) || path.extension() == Some(OsStr::new("cc")) {
+                paths.src_paths.push(path);
+            } else if path.extension() == Some(OsStr::new("h")) || path.extension() == Some(OsStr::new("hpp")) || path.extension() == Some(OsStr::new("hxx")) {
+                header_paths.push(path);
+            }
+        } else if file_type.is_dir() {
+            let path = entry.path();
+            let entries = fs::read_dir(&path).unwrap();
+            let child = header_and_src_paths(path, header_paths, entries);
+            paths.children.push(child);
+        }
+    }
+    paths
 }
 
 fn get_artifact_path(src_path: impl AsRef<Path>, output_dir_path: impl AsRef<Path>, extension: &'static str) -> PathBuf {
     let mut path = output_dir_path.as_ref().to_owned();
-    path.push(src_path.as_ref().file_stem().unwrap().to_str().unwrap().to_string());
+    path.push(src_path);
     let succ = path.set_extension(extension);
     assert!(succ);
     path
@@ -193,12 +279,6 @@ fn main() {
             }
         }
     }
-    macro_rules! fail {
-        ($($t:tt)*) => {
-            println!($($t)*);
-            success = false;
-        }
-    }
     macro_rules! fail_immediate {
         ($($t:tt)*) => {
             println!($($t)*);
@@ -213,6 +293,44 @@ fn main() {
     let compiler_path = r"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\bin\Hostx86\x86\cl.exe";
     let linker_path = r"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\bin\Hostx86\x86\link.exe";
     let compile_mode = CompileMode::Debug;
+    
+    let src_dir_path = Path::new("src");
+    let src_dir = match fs::read_dir(src_dir_path) {
+        Ok(src_dir) => src_dir,
+        Err(error) => {
+            if let IoErrorKind::NotFound = error.kind() {
+                fail_immediate!("src directory does not exist in current working directory.");
+            } else {
+                fail_immediate!("Unable to read src directory in current working directory.");
+            }
+        }
+    };
+    
+    // Create abs/debug or abs/release, if it doesn't exist already
+    let artifact_subdirectory = match compile_mode {
+        CompileMode::Debug => "debug",
+        CompileMode::Release => "release",
+    };
+    let artifact_path: PathBuf = ["abs", artifact_subdirectory].iter().collect();
+    let mut objs_path = artifact_path.clone();
+    objs_path.push("obj");
+    if let Some(kind) = fs::create_dir_all(&objs_path).err().map(|error| error.kind()) {
+        fail_immediate!(
+            "Unable to create abs directory structure: {:?}.",
+            match kind {
+                IoErrorKind::PermissionDenied => "permission denied".to_string(),
+                kind => format!("{:?}", kind),
+            }
+        );
+    }
+    
+    let mut header_paths = Vec::new();
+    let paths = header_and_src_paths(src_dir_path.to_path_buf(), &mut header_paths, src_dir);
+    
+    let newest_header = header_paths.iter().map(|header| {
+        fs::metadata(header).unwrap().last_write_time()
+    }).max().unwrap_or(0u64);
+    let mut obj_paths = Vec::new();
     let env = Environment::new(
         Host::Windows,
         compile_mode,
@@ -235,89 +353,12 @@ fn main() {
         &["_WINDOWS", "WIN32", "UNICODE", "_USE_MATH_DEFINES"],
         compiler_path,
         linker_path,
+        src_dir_path,
+        objs_path,
     );
-    let src_dir = match fs::read_dir("src") {
-        Ok(src_dir) => src_dir,
-        Err(error) => {
-            if let IoErrorKind::NotFound = error.kind() {
-                fail_immediate!("src directory does not exist in current working directory.");
-            } else {
-                fail_immediate!("Unable to read src directory in current working directory.");
-            }
-        }
-    };
 
-    // Create abs/debug or abs/release, if it doesn't exist already
-    let artifact_subdirectory = match compile_mode {
-        CompileMode::Debug => "debug",
-        CompileMode::Release => "release",
-    };
-    let artifact_path: PathBuf = ["abs", artifact_subdirectory].iter().collect();
-    let mut objs_path = artifact_path.clone();
-    objs_path.push("obj");
-    if let Some(kind) = fs::create_dir_all(&objs_path).err().map(|error| error.kind()) {
-        fail_immediate!(
-            "Unable to create abs directory structure: {:?}.",
-            match kind {
-                IoErrorKind::PermissionDenied => "permission denied".to_string(),
-                kind => format!("{:?}", kind),
-            }
-        );
-    }
+    success &= env.compile_directory(&paths, newest_header, &mut obj_paths);
 
-    let mut header_paths = Vec::new();
-    let mut src_paths = Vec::new();
-    for entry in src_dir {
-        let entry = entry.unwrap();
-        if entry.file_type().unwrap().is_file() {
-            let path = entry.path();
-            if path.extension() == Some(OsStr::new("cpp")) || path.extension() == Some(OsStr::new("cxx")) || path.extension() == Some(OsStr::new("cc")) {
-                src_paths.push(path);
-            } else if path.extension() == Some(OsStr::new("h")) || path.extension() == Some(OsStr::new("hpp")) || path.extension() == Some(OsStr::new("hxx")) {
-                header_paths.push(path);
-            }
-        } else if entry.file_type().unwrap().is_dir() {
-            // TODO: recursion
-        }
-    }
-
-    let mut obj_paths = Vec::new();
-    let newest_header = header_paths.iter().map(|header| {
-        fs::metadata(header).unwrap().last_write_time()
-    }).max().unwrap_or(0);
-    for (i, path) in src_paths.iter().enumerate() {
-        let obj_path = get_artifact_path(path, &objs_path, "obj");
-        obj_paths.push(obj_path.clone());
-        let mut needs_compile = true;
-        let src_modified = fs::metadata(path).unwrap().last_write_time();
-        if let Ok(metadata) = fs::metadata(&obj_path) {
-            let obj_modified = metadata.last_write_time();
-            if obj_modified > newest_header && obj_modified > src_modified {
-                needs_compile = false;
-            }
-        }
-        if needs_compile {
-            match env.compile(path, &objs_path) {
-                Ok(message) => print!("Compiled {}", message),
-                Err(error) => {
-                    fail!(
-                        "Attempted to compile {}Compilation failed{}.{}",
-                        error.message,
-                        if let Some(code) = error.code {
-                            format!(" with code {}", code)
-                        } else {
-                            String::new()
-                        },
-                        if i == src_paths.len()-1 {
-                            ""
-                        } else {
-                            "\n"
-                        }
-                    );
-                }
-            }
-        }
-    }
     check_success!();
     let lib_paths = &["avrt.lib"];
     if let Some(error) = env.link(&artifact_path, obj_paths, lib_paths).err() {
