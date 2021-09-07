f55a8dcfeeb10aea7e8422938d59a68d31be8c67
Zach Wolfe <zachrwolfe@me.com>
2021-09-07 01:42:30 -0500

    Add common control styles for GUI Win32 apps

--- src/build.rs
+++ src/build.rs
@@ -226,6 +226,13 @@ impl<'a> BuildEnvironment<'a> {
                         },
                     ).into()
                 );
+                match config.output_type {
+                    OutputType::GuiApp => {
+                        flags.push("/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0'
+                        processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'".into());
+                    }
+                    OutputType::ConsoleApp => {},
+                }
                 let mut dependencies = DependencyBuilder::default();
                 // TODO: Speed!!!
                 for lib in &config.link_libraries {
